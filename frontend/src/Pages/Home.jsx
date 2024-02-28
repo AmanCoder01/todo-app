2085a4afb7e7127891b586419a9029efb1cf3750
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-02-28 19:37:03 +0530

    first commit

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -61,15 +61,15 @@ const Home = ({ showModal, setShowModal }) => {
         <div className='  lg:w-7/12 sm:w-12/12  xs:w-12/12'>
           <div className='relative flex flex-col mx-auto bg-richblack-700 border-[0.3rem] border-richblack-200 max-w-[700px] min-w-[300px] h-[78vh] rounded-[1.2rem] my-4'>
             <div className='flex items-center justify-center space-x-6  py-8 mt-3'>
-              <button onClick={handleTasks} className={`bg-richblack-700 py-1 px-3 rounded-full min:w-[90px] w-32 border border-richblack-100 transition duration-200 text-richblack-25  
+              <button onClick={handleTasks} className={`bg-richblack-700 py-1 px-3 rounded-full min:w-[90px] lg:w-32 border border-richblack-100 transition duration-200 text-richblack-25  
 ${category === "Tasks" && "bg-richblack-800 py-2 border-2 border-richblack-200"
                 }
               `} >Tasks</button>
-              <button onClick={handlePending} className={`bg-richblack-700 py-1 px-3 rounded-full min:w-[90px] w-32 border border-richblack-100 transition duration-200 text-richblack-25  
+              <button onClick={handlePending} className={`bg-richblack-700 py-1 px-3 rounded-full min:w-[90px] lg:w-32 border border-richblack-100 transition duration-200 text-richblack-25  
 ${category === "Pending" && "bg-richblack-800 py-2 border-2 border-richblack-200"
                 }
               `} >Pending</button>
-              <button onClick={handleComplete} className={`bg-richblack-700 py-1 px-3 rounded-full min:w-[90px] w-32 border border-richblack-100 transition duration-200 text-richblack-25  
+              <button onClick={handleComplete} className={`bg-richblack-700 py-1 px-3 rounded-full min:w-[90px] lg:w-32 border border-richblack-100 transition duration-200 text-richblack-25  
 ${category === "Completed" && "bg-richblack-800 py-2 border-2 border-richblack-200"
                 }
               `}>Completed</button>
