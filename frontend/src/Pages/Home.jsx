10657fca6e12ffbe7f254289dc07323ef8a52114
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-02-28 20:22:46 +0530

    first commit

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -50,7 +50,7 @@ const Home = ({ showModal, setShowModal }) => {
       </div>
 
 
-      <div className='flex flex-wrap justify-center items-center xs:h-[90vh] h-[90vh] max-w-full '>
+      <div className='flex flex-wrap justify-center items-center xs:h-[80vh] h-[90vh] max-w-full '>
 
         <div className='lg:mt-[0rem] md:mt-[2rem] xs:mt-[3rem] sm:mt-[2rem] max-w-[580px] xs:w-[360px] lg:w-5/12 sm:w-12/12'>
           <h3 className='text-white text-xl font-semibold text-center sm:text-md'>Hi <i className='text-yellow-400 tracking-widest uppercase'>{name} </i> , Welcome to your task list...</h3>
@@ -59,7 +59,7 @@ const Home = ({ showModal, setShowModal }) => {
         </div>
 
         <div className='  lg:w-7/12 sm:w-12/12  xs:w-[360px]'>
-          <div className='relative flex flex-col mx-auto bg-richblack-700 border-[0.3rem] border-richblack-200 max-w-[700px] min-w-[270px] lg:h-[78vh] xs:h-[58vh] rounded-[1.2rem] my-4'>
+          <div className='relative flex flex-col mx-auto bg-richblack-700 border-[0.3rem] border-richblack-200 max-w-[700px] min-w-[270px] lg:h-[78vh] xs:h-[57vh] rounded-[1.2rem] my-4'>
             <div className='flex items-center justify-center space-x-6  py-8 xs:py-3 mt-3'>
               <button onClick={handleTasks} className={`bg-richblack-700 py-1 px-3 rounded-full min:w-[90px] lg:w-32 border border-richblack-100 transition duration-200 text-richblack-25  
 ${category === "Tasks" && "bg-richblack-800 py-2 border-2 border-richblack-200"
