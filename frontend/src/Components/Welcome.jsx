f13bae6cec8c7d6c2d7bd45d57ca410cb5fdc1be
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-02-28 16:29:49 +0530

    first commit

--- frontend/src/Components/Welcome.jsx
+++ frontend/src/Components/Welcome.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Welcome = () => {
     return (
-        <div className='flex flex-col gap-5 items-center h-[100vh] w-[100wh]'>
+        <div className='flex flex-col gap-5 items-center h-[100vh] w-[100wh] justify-center'>
             <h1 className="text-center text-4xl font-bold text-richblack-5">
                 Welcome to the ToDo App!
             </h1>
 
-            <button className="bg-yellow-400 hover:bg-yellow-500 transition duration-300 text-richblack-700 font-bold py-[7px] px-5 rounded-lg"
-            >Signup</button>
+            <Link to="/signup">
+                <button className="bg-yellow-400 hover:bg-yellow-500 transition duration-300 text-richblack-700 font-bold py-[7px] px-5 rounded-lg"
+                >Signup</button></Link>
         </div>
     )
 }
